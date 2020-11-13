68b7663143e215272a247b1b02892126acea07ec
Danil <56827297+fire1fly@users.noreply.github.com>
2020-11-14 00:32:46 +0800

    edit user information, sign up and login validation

--- js/main.js
+++ js/main.js
@@ -6,15 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
         menu = document.querySelector('.sidebar'),
         loginElem = document.querySelector(".login"),
         userElem = document.querySelector(".user"),
+        userAvatarElem = userElem.querySelector(".user-avatar"),
         userNameElem = userElem.querySelector(".user-name"),
         sidebarNavElem = document.querySelector(".sidebar-nav"),
         btnNewPost = document.querySelector(".button-new-post"),
         loginForm = document.querySelector(".login-form"),
         emailInput = loginForm.querySelector(".login-email"),
         passInput = loginForm.querySelector(".login-pass"),
-        signupLink = loginForm.querySelector(".login-signup");
+        signupLink = loginForm.querySelector(".login-signup"),
+        btnExit = userElem.querySelector(".exit"),
+        iconEdit = userElem.querySelector(".icon-edit"),
+        userEditElem = userElem.querySelector(".user-edit"),
+        userEditForm = userElem.querySelector(".edit-form"),
+        userEditUsername = userElem.querySelector(".edit-username"),
+        userEditAvatar = userElem.querySelector(".edit-avatar");
 
   // data
+
+  const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 
   const usersList = [
     {
@@ -37,6 +46,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
       console.log('logIn');
 
+      if(!regExpValidEmail.test(email)) return alert("Невалидная почта.");
+
       const user = this.getUser(email)
 
       if (user && user.pass === pass) {
@@ -44,32 +55,49 @@ document.addEventListener("DOMContentLoaded", function() {
         handler();
       } else {
         alert('Неверный email или пароль');
-        emailInput.value = '';
-        passInput.value = '';
       }
 
     },
-    logOut() {
-      console.log('logOut');
+    logOut(handler) {
+      this.user = null;
+      handler();
     },
     signUp(email, pass, handler) {
       console.log('signUp');
 
-      if (!this.getUser(email)) {
-        let displayName = email.slice(0, email.indexOf("@"));
-        const user = {email, pass, displayName};
+      if(!regExpValidEmail.test(email)) return alert("Невалидное или пустое поле email.");
 
-        usersList.push(user);
-        this.authorizedUser(user);
-        handler();
+      if (pass.trim() !== '') {
+        if (!this.getUser(email)) {
+          let displayName = email.substring(0, email.indexOf("@"));
+          const user = {email, pass, displayName, photo: ''};
 
-      } else {
-        console.error('Пользователь с таким email уже существует');
-        alert('Пользователь с таким email уже существует');
-        emailInput.value = '';
-        passInput.value = '';
+          
+          usersList.push(user);
+          this.authorizedUser(user);
+          handler();
+
+        } else {
+          console.error('Пользователь с таким email уже существует.');
+          alert('Пользователь с таким email уже существует.');
+        }       
+      } else if (pass === '') {
+        alert("Вы не ввели пароль");
       }
-      
+    },
+    editUser(username, avatar = '', handler) {
+      if (username) {
+        this.user.displayName = username;
+      }
+
+      if (avatar) {
+        this.user.photo = avatar;
+      }
+
+      handler();
+
+      console.log(this.user);
+
     },
     getUser(email) {
       return usersList.find((item) => item.email == email);
@@ -84,13 +112,13 @@ document.addEventListener("DOMContentLoaded", function() {
     if (user) {
       loginElem.style.display = 'none';
       userElem.style.display = '';
-      userNameElem.textContent = user.displayName;
+      userNameElem.textContent = user.displayName || userNameElem.textContent;
       sidebarNavElem.style.display = 'block';
       btnNewPost.style.display = 'flex';
+      userAvatarElem.src = user.photo || userAvatarElem.src;
     } else {
       loginElem.style.display = '';
       userElem.style.display = 'none';
-      userNameElem.textContent = null;
       sidebarNavElem.style.display = '';
       btnNewPost.style.display = '';
     }
@@ -112,6 +140,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setUsers.logIn(emailInput.value, passInput.value, toggleAuthDom);
 
+    loginForm.reset();
+
   });
 
   signupLink.addEventListener("click", event => {
@@ -120,6 +150,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setUsers.signUp(emailInput.value, passInput.value, toggleAuthDom);
 
+    loginForm.reset();
+
+  });
+
+  btnExit.addEventListener("click", event => {
+
+    event.preventDefault();
+
+    setUsers.logOut(toggleAuthDom);
+
+  });
+
+  iconEdit.addEventListener("click", event => {
+    event.preventDefault();
+
+    userEditElem.classList.toggle("visible");
+    
+    userEditUsername.value = setUsers.user.displayName;
+
+  });
+
+  userEditForm.addEventListener("submit", event => {
+
+    event.preventDefault();
+
+    setUsers.editUser(userEditUsername.value, userEditAvatar.value, toggleAuthDom);
+
+    userEditElem.classList.remove("visible");
   });
 
   toggleAuthDom();
