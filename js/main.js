3ba3aac53be13444905e26b04f3a28b141fb00f1
Danil <56827297+fire1fly@users.noreply.github.com>
2020-11-14 23:36:41 +0800

    adding posts, sync. changing user info

--- js/main.js
+++ js/main.js
@@ -19,21 +19,25 @@ document.addEventListener("DOMContentLoaded", function() {
         userEditElem = userElem.querySelector(".user-edit"),
         userEditForm = userElem.querySelector(".edit-form"),
         userEditUsername = userElem.querySelector(".edit-username"),
-        userEditAvatar = userElem.querySelector(".edit-avatar");
+        userEditAvatar = userElem.querySelector(".edit-avatar"),
+        postsWrapper = document.querySelector(".posts"),
+        addPostForm = document.querySelector(".add-post"),
+        addTextPostInput = addPostForm.querySelector(".add-text"),
+        addTextSymbolCounter = addPostForm.querySelector(".add-text__counter");
 
   // data
 
-  const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
+  const REGEXP_MAIL_VALID = /^\w+@\w+\.\w{2,}$/;
 
   const usersList = [
     {
-      id: '01',
+      id: 1,
       email: 'den@mail.com',
       pass: 'qwerty',
       displayName: 'DenJs'
     },
     {
-      id: '02',
+      id: 2,
       email: 'alex@gmail.com',
       pass: '12345',
       displayName: 'AlexC#'
@@ -46,9 +50,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
       console.log('logIn');
 
-      if(!regExpValidEmail.test(email)) return alert("Невалидная почта.");
+      if(!REGEXP_MAIL_VALID.test(email)) {
+        alert("Невалидное поле email. \n Логин почты не может содержать спец.символы");
+        return;
+      }
 
-      const user = this.getUser(email)
+      const user = this.getUser(email);
 
       if (user && user.pass === pass) {
         this.authorizedUser(user);
@@ -65,45 +72,116 @@ document.addEventListener("DOMContentLoaded", function() {
     signUp(email, pass, handler) {
       console.log('signUp');
 
-      if(!regExpValidEmail.test(email)) return alert("Невалидное или пустое поле email.");
+      if(!REGEXP_MAIL_VALID.test(email)) {
+        alert("Невалидное поле email. \n Логин почты не может содержать спец.символы");
+        return;
+      }
 
       if (pass.trim() !== '') {
         if (!this.getUser(email)) {
           let displayName = email.substring(0, email.indexOf("@"));
-          const user = {email, pass, displayName, photo: ''};
-
-          
+          const user = {id : usersList.length + 1,email, pass, displayName, photoURL: ''};
           usersList.push(user);
           this.authorizedUser(user);
-          handler();
+          console.log(usersList);
+          if (handler) {
+            handler();
+          }
 
         } else {
-          console.error('Пользователь с таким email уже существует.');
           alert('Пользователь с таким email уже существует.');
         }       
-      } else if (pass === '') {
+      } else {
         alert("Вы не ввели пароль");
       }
     },
     editUser(username, avatar = '', handler) {
       if (username) {
+        if (username.length < 3 || username.length > 11 ) {
+          return alert("Недопустимая длина имени. Имя пользователя может содержать от 3 до 11 символов.")
+        }
         this.user.displayName = username;
       }
 
       if (avatar) {
-        this.user.photo = avatar;
+        this.user.photoURL = avatar;
       }
 
-      handler();
+      if (handler) {
+        handler();
+      }
 
-      console.log(this.user);
+      setPosts.updateUserDataInPosts();
+      showAllPosts();
 
     },
     getUser(email) {
-      return usersList.find((item) => item.email == email);
+      return usersList.find(item => item.email == email);
     },
     authorizedUser(user) {
       this.user = user;
+    }
+  }
+
+  const setPosts = {
+    posts: [
+      {
+        title: 'Заголовок этого поста',
+        text: `Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот
+        маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему
+        букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его
+        снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую
+        подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что
+        вопроса ведущими о решила одна алфавит!`,
+        tags: ['свежее','новое','горячее','мое','случай'],
+        author: {id: 1, displayName: 'DenJS', photoURL: ''},
+        date: '11.11.20, 14.05',
+        likes: 85,
+        comments: 20
+      },
+      {
+        title: 'Заголовок этого поста №2',
+        text: `Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот
+        маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему
+        букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его
+        снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую
+        подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что
+        вопроса ведущими о решила одна алфавит!`,
+        tags: ['старческое','холодненькое','мое','привет'],
+        author: {id: 2, displayName: 'atrem goncharov', photoURL: ''},
+        date: '11.11.20, 14.05',
+        likes: 26,
+        comments: 6
+      }
+    ],
+    addPost(title, text, tags, handler) {
+
+      this.posts.unshift({
+        title,
+        text,
+        tags: tags.split(',').map((item) => item.trim()),
+        author: {
+          id: setUsers.user.id,
+          displayName: setUsers.user.displayName,
+          photoURL: setUsers.user.photoURL
+        },
+        date: new Date().toLocaleDateString(),
+        likes: 0,
+        comments: 0
+      });
+      
+      if (handler) {
+        handler();
+      }
+
+      console.log(this.posts);
+
+    },
+    updateUserDataInPosts() {
+      this.posts.forEach(post => {
+        post.author.displayName = usersList[post.author.id - 1].displayName;
+        post.author.photoURL = usersList[post.author.id - 1].photoURL;
+      });
     }
   }
 
@@ -114,73 +192,194 @@ document.addEventListener("DOMContentLoaded", function() {
       userElem.style.display = '';
       userNameElem.textContent = user.displayName || userNameElem.textContent;
       sidebarNavElem.style.display = 'block';
-      btnNewPost.style.display = 'flex';
-      userAvatarElem.src = user.photo || userAvatarElem.src;
+      btnNewPost.classList.add("visible");
+      userAvatarElem.src = user.photoURL || userAvatarElem.src;
     } else {
       loginElem.style.display = '';
       userElem.style.display = 'none';
       sidebarNavElem.style.display = '';
-      btnNewPost.style.display = '';
+      btnNewPost.classList.remove("visible");
+      addPostForm.classList.remove("visible");
+      postsWrapper.classList.add("visible");
     }
   }
 
-  // events and func calls
+  const showAddPost = () => {
+    addPostForm.classList.add("visible");
+    postsWrapper.classList.remove("visible");
+  }
   
-  menuToggle.addEventListener('click', function (event) {
+  const showAllPosts = () => {
 
-    event.preventDefault();
+    setPosts.updateUserDataInPosts();
 
-    menu.classList.toggle('visible');
+    let postsMarkup = '';
 
-  });
+    setPosts.posts.forEach(({title, text, tags, author, date, likes, comments}) => {
+      postsMarkup += `<section class="post">
+                        <div class="post-body">
+                          <h2 class="post-title">${title}</h2>
+                          <p class="post-text">${text}</p>
+                          <div class="tags">${tags.map(tag => `<a href="#${tag}" class="tag">#${tag}</a>`).join('')}</div>
+                          <!-- /.tags -->
+                        </div>
+                        <!-- /.post-body -->
+                        <div class="post-footer">
+                          <div class="post-buttons">
+                            <button class="post-button likes">
+                              <svg width="19" height="20" class="icon icon-like">
+                                <use xlink:href="img/icons.svg#like"></use>
+                              </svg>
+                              <span class="likes-counter">${likes}</span>
+                            </button>
+                            <button class="post-button comments">
+                              <svg width="21" height="21" class="icon icon-comment">
+                                <use xlink:href="img/icons.svg#comment"></use>
+                              </svg>
+                              <span class="comments-counter">${comments}</span>
+                            </button>
+                            <button class="post-button save">
+                              <svg width="19" height="19" class="icon icon-save">
+                                <use xlink:href="img/icons.svg#save"></use>
+                              </svg>
+                            </button>
+                            <button class="post-button share">
+                              <svg width="17" height="19" class="icon icon-share">
+                                <use xlink:href="img/icons.svg#share"></use>
+                              </svg>
+                            </button>
+                          </div>
+                          <!-- /.post-buttons -->
+                          <div class="post-author">
+                            <div class="author-about">
+                              <a href="#" class="author-username">${author.displayName}</a>
+                              <span class="post-time">${date}</span>
+                            </div>
+                            <a href="#" class="author-link"><img src="${author.photoURL || 'img/user-avatar.png'}" alt="avatar" class="author-avatar"></a>
+                          </div>
+                          <!-- /.post-author -->
+                        </div>
+                        <!-- /.post-footer -->
+                      </section>`
+    });
 
-  loginForm.addEventListener("submit", event => {
+    postsWrapper.innerHTML = postsMarkup;
 
-    event.preventDefault();
-
-    setUsers.logIn(emailInput.value, passInput.value, toggleAuthDom);
-
-    loginForm.reset();
-
-  });
-
-  signupLink.addEventListener("click", event => {
-
-    event.preventDefault();
-
-    setUsers.signUp(emailInput.value, passInput.value, toggleAuthDom);
-
-    loginForm.reset();
-
-  });
-
-  btnExit.addEventListener("click", event => {
-
-    event.preventDefault();
-
-    setUsers.logOut(toggleAuthDom);
-
-  });
-
-  iconEdit.addEventListener("click", event => {
-    event.preventDefault();
-
-    userEditElem.classList.toggle("visible");
+    addPostForm.classList.remove("visible");
+    postsWrapper.classList.add("visible");
     
-    userEditUsername.value = setUsers.user.displayName;
+  }
 
-  });
+  // init
 
-  userEditForm.addEventListener("submit", event => {
+  const init = () => {
 
-    event.preventDefault();
+    menuToggle.addEventListener('click', function (event) {
 
-    setUsers.editUser(userEditUsername.value, userEditAvatar.value, toggleAuthDom);
+      event.preventDefault();
+  
+      menu.classList.toggle('visible');
+  
+    });
+  
+    loginForm.addEventListener("submit", event => {
+  
+      event.preventDefault();
+  
+      setUsers.logIn(emailInput.value, passInput.value, toggleAuthDom);
+  
+      loginForm.reset();
+  
+    });
+  
+    signupLink.addEventListener("click", event => {
+  
+      event.preventDefault();
+  
+      setUsers.signUp(emailInput.value, passInput.value, toggleAuthDom);
+  
+      loginForm.reset();
+  
+    });
+  
+    btnExit.addEventListener("click", event => {
+  
+      event.preventDefault();
+  
+      setUsers.logOut(toggleAuthDom);
+  
+    });
+  
+    iconEdit.addEventListener("click", event => {
 
-    userEditElem.classList.remove("visible");
-  });
+      event.preventDefault();
+  
+      userEditElem.classList.toggle("visible");
+      
+      userEditUsername.value = setUsers.user.displayName;
+  
+    });
+  
+    userEditForm.addEventListener("submit", event => {
+  
+      event.preventDefault();
+  
+      setUsers.editUser(userEditUsername.value, userEditAvatar.value, toggleAuthDom);
+  
+      userEditElem.classList.remove("visible");
+  
+    });
 
-  toggleAuthDom();
+    btnNewPost.addEventListener("click", event => {
+
+      event.preventDefault();
+
+      showAddPost();
+
+    });
+
+    addPostForm.addEventListener("submit", event => {
+
+      event.preventDefault();
+
+      const formElements = addPostForm.elements;
+
+      const {title, text, tags} = formElements;
+
+      console.log(title, text, tags);
+
+      if (title.value.length < 3) {
+        alert("Заголовок поста не может быть меньше 3 символов");
+        return;
+      }
+
+      if (text.value.length < 3 || text.value.length > 1600) {
+        alert("Длина поста не может быть меньше 3 и не больше 1600 символов");
+        return;
+      }
+
+      setPosts.addPost(title.value, text.value, tags.value, showAddPost);
+
+      showAllPosts();
+
+      addPostForm.classList.remove("visible");
+
+      addPostForm.reset();
+
+    });
+
+    addTextPostInput.addEventListener("input", event => {
+      addTextSymbolCounter.textContent = addTextPostInput.value.length;
+    });
+  
+
+    showAllPosts(); 
+  
+    toggleAuthDom();
+
+  }
+
+  init();
 
 });
 
